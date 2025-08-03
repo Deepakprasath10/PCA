b3368929abcce1c45e14c14bd72680bff7ca8e54
Deepakprasath10 <deepakprasath02@gmail.com>
2025-08-03 10:55:35 +0530

    files added.

--- model.py
+++ model.py
@@ -4,17 +4,14 @@ from sklearn.decomposition import PCA
 from sklearn.impute import SimpleImputer
 
 def apply_pca(df):
-    df = df.drop(columns=["CustomerID"], errors='ignore')  # Optional identifier removal
-
-    # Handle missing values
+    df = df.drop(columns=["CustomerID"], errors='ignore') 
+  
     imputer = SimpleImputer(strategy='mean')
     df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
-    # Standardize data
     scaler = StandardScaler()
     scaled_data = scaler.fit_transform(df_imputed)
 
-    # PCA
     pca = PCA(n_components=2)
     principal_components = pca.fit_transform(scaled_data)
     pca_df = pd.DataFrame(data=principal_components, columns=["PC1", "PC2"])
